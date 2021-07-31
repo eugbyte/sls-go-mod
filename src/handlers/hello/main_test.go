e3064c87056ac93d60464425c6c6c15b0f1a458d
eugbyte <eugenetham1994@gmail.com>
2021-07-31 21:36:18 +0800

    chore: fix

--- src/handlers/hello/main_test.go
+++ src/handlers/hello/main_test.go
@@ -8,8 +8,6 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-	t.FailNow()
-
 	mockRequest := Request{}
 	mockRequest.Body = util.Stringify(RequestBody{Message: "Hello"})
 	response, err := Handler(mockRequest)
